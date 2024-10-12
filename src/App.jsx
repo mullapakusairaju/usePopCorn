e0138694946c75cf3dd1b6864856a94a6bd222f8
mullapakusairaju <76658260+mullapakusairaju@users.noreply.github.com>
2024-10-12 18:57:19 +0530

    added img

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,7 @@ function App() {
       Id: 3,
       Title: "Dilwale Dulhania Le Jayenge",
       Date: "2020-09-12",
-      img: "https://thumbs.dreamstime.com/b/logo-deadpool-black-background-d-comic-hero-marvel-vector-illustration-144205291.jpg",
-    },
-    {
-      Id: 4,
-      Title: "365 Days",
-      Date: "2020-09-12",
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVYOpbpPYBKKLGmdib61vLeTkUenmlC_8m0MHSsOswXdnJ7a6fgQTLwQG9P0bwa0PL_8&usqp=CAU",
+      img: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p25126_p_v8_ac.jpg",
     },
   ];
 
